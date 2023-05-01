94b403c5b291f8dad863324ba97321c7d6ae63ff
kristencai <kristen_cai@brown.edu>
2023-05-01 16:56:31 -0400

    model

--- code/model.py
+++ code/model.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import numpy as np
 from re import X
-from keras.applications.resnet50 import ResNet50
+from keras.applications import ResNet50
 
-# from keras.applications.resnet import ResNet152
+from keras.applications.resnet import ResNet152
 from keras.layers import Dense, Flatten, Dropout
 from keras import Sequential
 from keras.models import Model
@@ -13,30 +13,11 @@ from preprocess import get_labels, unpickle
 from re import X
 
 def train_model(images, one_hots):
-    # cnn_model = tf.keras.Sequential([
-    #         tf.keras.layers.Conv2D(64,2),
-    #         tf.keras.layers.Conv2D(64,2),
-    #         tf.keras.layers.MaxPool2D(),
-    #         tf.keras.layers.Conv2D(32,8),
-    #         tf.keras.layers.Conv2D(32,8),
-    #         tf.keras.layers.Conv2D(32,8),
-    #         tf.keras.layers.AveragePooling2D(),
-    #         tf.keras.layers.Flatten(),
-    #         tf.keras.layers.Dense(128, activation="relu"),
-    #         tf.keras.layers.Dense(10, activation="softmax")])
-    # cnn_model = tf.keras.applications.resnet.ResNet152()
-
-    # cnn_model.compile(
-    #     optimizer=tf.keras.optimizers.Adam(),
-    #     loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-    #     metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
-
-
-
 
     # load in the pre-trained resnet
-    # resnet152 = ResNet152(include_top=False, weights='imagenet', input_shape=(256,256,3))
-    resnet50 = ResNet50(include_top=False, weights='imagenet', input_shape=(256,256,3))
+    # resnet50 = ResNet50(include_top=False, weights='imagenet', input_shape=(256,256,3))
+    resnet50 = ResNet152(include_top=False, weights='imagenet', input_shape=(256,256,3))
+    
 
     # freeze the pre-trained layers, and only train the newly added layers
     # for layer in resnet152.layers:
@@ -60,8 +41,10 @@ def train_model(images, one_hots):
 
     flatten=Flatten()(resnet50.output)
     dense1=Dense(128, activation='relu')(flatten)
-    dense2=Dense(64, activation='relu')(dense1)
-    predictions=Dense(2, activation='sigmoid')(dense2)
+    dropout1=Dropout(rate=0.3)(dense1)
+    dense2=Dense(64, activation='relu')(dropout1)
+    dropout2=Dropout(rate=0.3)(dense2)
+    predictions=Dense(2, activation='sigmoid')(dropout2)
 
 
     model = Model(inputs=resnet50.input, outputs=predictions)
@@ -72,10 +55,10 @@ def train_model(images, one_hots):
 
 
     # shuffle 
-    # indices = tf.range(start=0, limit=len(one_hots))
-    # idx = tf.random.shuffle(indices)
-    # images = tf.gather(images, idx)
-    # one_hots = tf.gather(one_hots, idx)
+    indices = tf.range(start=0, limit=len(one_hots))
+    idx = tf.random.shuffle(indices)
+    images = tf.gather(images, idx)
+    one_hots = tf.gather(one_hots, idx)
 
     # # train the models
     # history = model.fit(train_dataset, validation_data=val_dataset, epochs=10)
@@ -83,23 +66,8 @@ def train_model(images, one_hots):
     # print(one_hots)
     print(images[:1904].shape)
     print(one_hots[:1904].shape)
-    history = model.fit(images[:1904], one_hots[:1904], batch_size=32, epochs=10, 
+    history = model.fit(images[:1904], one_hots[:1904], batch_size=128, epochs=3, 
                         validation_data=(images[1904:], one_hots[1904:]))
-
-
-
-    '''
-    # Shuffle
-    # indices = tf.range(start=0, limit=len(labels))
-    # idx = tf.random.shuffle(indices)
-    # inputs = tf.gather(inputs, idx)
-    # labels = tf.gather(labels, idx)
-
-    # # Train
-    # history = cnn_model.fit(images[:1904], one_hots[:1904], batch_size=32, epochs=15, 
-    #                     validation_data=(images[1904:], one_hots[1904:]))
-    '''
-
 
 
 
