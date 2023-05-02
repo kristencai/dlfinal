aa859d2d9c11f196423cb1d9a57d389403243125
“syu0916” <sean_yu@brown.edu>
2023-05-02 18:06:52 -0400

    ASDF

--- code/preprocess.py
+++ code/preprocess.py
@@ -2,7 +2,7 @@ import pickle
 import tensorflow as tf
 import numpy as np
 from tqdm import tqdm
-from PIL import Image
+from PIL import Image, ImageOps
 import os
 
 def get_labels():
@@ -40,109 +40,117 @@ def get_labels():
     print(f'Benign: {labels.count(0)}')
     print(f'Malignant: {labels.count(1)}')
 
-# def preprocess_images():
-#     labels = []
-#     id_to_label = {}
-#     line_count = 0
+def preprocess_images():
+    labels = []
+    id_to_label = {}
+    line_count = 0
 
 
-#     # The malignant or benign classifier is located at index 6 of the csv!
-#     # though, it can be 7 sometimes, depending on how many commmas a name has.
-#     with open('data/metadata', 'r') as file:
-#         for line in file:
-#             line_count += 1
-#             if line_count == 1:
-#                 continue
-#             line_array = line.split(",")
+    # The malignant or benign classifier is located at index 6 of the csv!
+    # though, it can be 7 sometimes, depending on how many commmas a name has.
+    with open('data/metadata', 'r') as file:
+        for line in file:
+            line_count += 1
+            if line_count == 1:
+                continue
+            line_array = line.split(",")
 
-#             if 'benign' in line_array:
-#                 id_to_label[line_array[0]] = 0
-#             elif 'malignant' in line_array:
-#                 id_to_label[line_array[0]] = 1
+            if 'benign' in line_array:
+                id_to_label[line_array[0]] = 0
+            elif 'malignant' in line_array:
+                id_to_label[line_array[0]] = 1
 
             
-#     print(line_count)
-#     labels = list(id_to_label.values())
+    print(line_count)
+    labels = list(id_to_label.values())
 
 
-#     ############################## PRINTING AID! ###################################
-#     # for key in id_to_label.keys():
-#     #   if id_to_label[key] != 'malignant' and id_to_label[key] != 'benign':
-#     #     print(f'this is the key {key} and value {id_to_label[key]}')
-#     ################################################################################
-#     # print(f'Labels: {labels}')
-#     print(f'Total labels count: {len(labels)}')
-#     print(f'Benign: {labels.count(0)}')
-#     print(f'Malignant: {labels.count(1)}')
+    ############################## PRINTING AID! ###################################
+    # for key in id_to_label.keys():
+    #   if id_to_label[key] != 'malignant' and id_to_label[key] != 'benign':
+    #     print(f'this is the key {key} and value {id_to_label[key]}')
+    ################################################################################
+    # print(f'Labels: {labels}')
+    print(f'Total labels count: {len(labels)}')
+    print(f'Benign: {labels.count(0)}')
+    print(f'Malignant: {labels.count(1)}')
 
 
-#     directory = 'images'
+    directory = 'images'
 
-#     # TODO: Try using a conv2d layer with multiple output channels to extract the 
-#     # rough features of the image and use that as input as well
-
-
-#     # picture_id -> (pixel values, label value)
-#     data = {}
-#     count = 0
-#     malignant = 0
-#     benign = 0
-#     # this is the final code bc it resizes all of them
-#     # for each photo, we resize, get the pixel values, and reshape to (256, 256, 3)
-#     for filename in tqdm(os.scandir(directory)):
-#         # count += 1
-#         # if count > 100:
-#         #   break
-#         if filename.is_file():
-#             with Image.open(filename.path) as img:
-#             # print(filename.path.strip('images/').strip('.JPG'))
-#                 if filename.path.strip('images/').strip('.JPG') in id_to_label:
-#                     if id_to_label[filename.path.strip('images/').strip('.JPG')] == 1:
-#                         new_image = img.resize((256,256))
-#                         # new_image.show()
-
-#                         ninety = new_image.rotate(90.0)
-#                         # ninety.show()
-#                         ninety = np.array(list(ninety.getdata()))
-#                         ninety = np.reshape(ninety, (256, 256, 3))
-
-#                         one_eighty = new_image.rotate(180.0)
-#                         # one_eighty.show()
-#                         one_eighty = np.array(list(one_eighty.getdata()))
-#                         one_eighty = np.reshape(one_eighty, (256, 256, 3))
+    # TODO: Try using a conv2d layer with multiple output channels to extract the 
+    # rough features of the image and use that as input as well
 
 
-#                         two_seventy = new_image.rotate(270.0)     
-#                         # two_seventy.show()
-#                         two_seventy = np.array(list(two_seventy.getdata()))
-#                         two_seventy = np.reshape(two_seventy, (256, 256, 3))
+    # picture_id -> (pixel values, label value)
+    data = {}
+    count = 0
+    malignant = 0
+    benign = 0
+    # this is the final code bc it resizes all of them
+    # for each photo, we resize, get the pixel values, and reshape to (256, 256, 3)
+    for filename in tqdm(os.scandir(directory)):
+        # count += 1
+        # if count > 100:
+        #   break
+        if filename.is_file():
+            with Image.open(filename.path) as img:
+            # print(filename.path.strip('images/').strip('.JPG'))
+                if filename.path.strip('images/').strip('.JPG') in id_to_label:
+                    if id_to_label[filename.path.strip('images/').strip('.JPG')] == 1:
+                        new_image = img.resize((256,256))
+                        # new_image.show()
 
-#                         new_image = np.array(list(new_image.getdata()))
-#                         new_image = np.reshape(new_image, (256, 256, 3))
-#                         data[filename.path.strip('images/').strip('.JPG')] = \
-#                         ([new_image , ninety, one_eighty, two_seventy], id_to_label[filename.path.strip('images/').strip('.JPG')])
-#                         malignant += 4
-#                     else: 
-#                         new_image = img.resize((256,256))
+                        ninety = new_image.rotate(90.0)
+                        # ninety.show()
+                        ninety = np.array(list(ninety.getdata()))
+                        ninety = np.reshape(ninety, (256, 256, 3))
 
-#                         one_eighty = new_image.rotate(180.0)
-#                         # one_eighty.show()
-#                         one_eighty = np.array(list(one_eighty.getdata()))
-#                         one_eighty = np.reshape(one_eighty, (256, 256, 3))
+                        one_eighty = new_image.rotate(180.0)
+                        # one_eighty.show()
+                        one_eighty = np.array(list(one_eighty.getdata()))
+                        one_eighty = np.reshape(one_eighty, (256, 256, 3))
 
-#                         new_image = np.array(list(new_image.getdata()))
-#                         new_image = np.reshape(new_image, (256, 256, 3))
-#                         data[filename.path.strip('images/').strip('.JPG')] = \
-#                         ([new_image, one_eighty] , id_to_label[filename.path.strip('images/').strip('.JPG')])
-#                         benign += 2
 
-#     print(f' overall malignant: {malignant} v. benign: {benign}')
+                        two_seventy = new_image.rotate(270.0)     
+                        # two_seventy.show()
+                        two_seventy = np.array(list(two_seventy.getdata()))
+                        two_seventy = np.reshape(two_seventy, (256, 256, 3))
 
-#     with open(f'data/data.npy', 'wb') as pickle_file:
-#         np.save( pickle_file, data)
-#     print(f'Data has been dumped into data.npy!')
+                        invert = ImageOps.invert(new_image)
+                        invert = np.array(list(invert.getdata()))
+                        invert = np.reshape(invert, (256, 256, 3))
 
-def preprocess_images():
+                        new_image = np.array(list(new_image.getdata()))
+                        new_image = np.reshape(new_image, (256, 256, 3))
+                        data[filename.path.strip('images/').strip('.JPG')] = \
+                        ([new_image , ninety, one_eighty, two_seventy, invert], id_to_label[filename.path.strip('images/').strip('.JPG')])
+                        malignant += 4
+                    else: 
+                        new_image = img.resize((256,256))
+
+                        one_eighty = new_image.rotate(180.0)
+                        # one_eighty.show()
+                        one_eighty = np.array(list(one_eighty.getdata()))
+                        one_eighty = np.reshape(one_eighty, (256, 256, 3))
+
+                        invert = ImageOps.invert(new_image)
+                        invert = np.array(list(invert.getdata()))
+                        invert = np.reshape(invert, (256, 256, 3))
+
+                        new_image = np.array(list(new_image.getdata()))
+                        new_image = np.reshape(new_image, (256, 256, 3))
+                        data[filename.path.strip('images/').strip('.JPG')] = \
+                        ([new_image, one_eighty, invert] , id_to_label[filename.path.strip('images/').strip('.JPG')])
+                        benign += 2
+
+    print(f' overall malignant: {malignant} v. benign: {benign}')
+
+    with open(f'data/data.npy', 'wb') as pickle_file:
+        np.save( pickle_file, data)
+    print(f'Data has been dumped into data.npy!')
+
+# def preprocess_images():
     labels = []
     id_to_label = {}
     line_count = 0
@@ -229,7 +237,7 @@ def preprocess_images():
 
 
 
-def unpickle_vgg():
+# def unpickle_vgg():
     data = {
     }
     with open('data/vggpreprocess.npy', 'rb') as file:
