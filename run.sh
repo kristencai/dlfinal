aba59acc27057154a061b977a11a0207a82f7615
“syu0916” <sean_yu@brown.edu>
2023-05-01 23:56:59 -0400

    ASDF

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 source /course/cs1470/cs1470_env/bin/activate
-python3 /home/syu66/CS1470/dlfinal/code/model.py
+cd /home/syu66/CS1470/dlfinal
+python3 code/model.py
 #% qsub -l gpus=2 -l gmem=24 runme
